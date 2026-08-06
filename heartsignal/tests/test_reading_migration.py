0afad7dc3dbfbcac8bd0807bc537445920c2bde7
vladimirutkin2mn-lgtm <vladimir.utkin2mn@gmail.com>
2026-08-06 12:43:51 +0300

    Update reading migration test head

--- heartsignal/tests/test_reading_migration.py
+++ heartsignal/tests/test_reading_migration.py
@@ -11,7 +11,7 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
 
 pytestmark = pytest.mark.postgres
-_HEAD = "20260806_19"
+_HEAD = "20260806_20"
 _PARENT = "20260805_16"
 
 
