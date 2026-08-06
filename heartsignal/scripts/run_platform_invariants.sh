4d0be2883d74040b9fd827c5033c7e8340cbbbc3
vladimirutkin2mn-lgtm <vladimir.utkin2mn@gmail.com>
2026-08-06 18:34:26 +0300

    test: protect Love Oracle vertical invariants

--- heartsignal/scripts/run_platform_invariants.sh
+++ heartsignal/scripts/run_platform_invariants.sh
@@ -42,4 +42,6 @@ pytest \
   tests/test_reading_generation_memory.py::test_frozen_v1_reading_never_retrieves_or_serializes_memory \
   tests/test_reading_generation_memory.py::test_memory_retrieval_failure_does_not_block_reading_generation \
   tests/test_reading_memory_usage.py::test_usage_telemetry_failure_keeps_context_and_logs_no_value \
+  tests/test_love_oracle_reading.py::test_postgres_love_oracle_preview_is_validated_and_idempotent \
+  tests/test_love_oracle_reading.py::test_mind_reading_and_guaranteed_reunion_are_rejected_before_persistence \
   "$@"
